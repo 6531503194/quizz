e411159bb69dcd25f009080231e919385e54616f
6531503194 <6531503194@lamduan.mfu.ac.th>
2025-01-03 01:09:48 +0700

    Make navigation in Learning_zone pages

---
+++ quizz/src/main/java/com/edtech/quizz/Controller/FroentEndTestingController.java
@@ -0,0 +1,30 @@
+package com.edtech.quizz.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class FroentEndTestingController {
+    
+    @GetMapping({"/","/home"})
+    public String homePage(){
+        return "home";
+    }
+
+    @GetMapping("/learning_zone")
+    public String learningZone(){
+        return "learning_zone";
+    }
+
+    @GetMapping("/card")
+    public String cardList(){
+        return "card";
+    }
+
+    @GetMapping("/card_collection")
+    public String favIcon(){
+        return "card_collection";
+    }
+
+}
