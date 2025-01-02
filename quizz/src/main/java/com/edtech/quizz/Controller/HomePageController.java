ada0c656fe11c9719b993b840f5298eb6f433765
6531503194 <6531503194@lamduan.mfu.ac.th>
2025-01-02 23:55:23 +0700

    Icons,Images and front-end files uploaded & home page test with controller

---
+++ quizz/src/main/java/com/edtech/quizz/Controller/HomePageController.java
@@ -0,0 +1,14 @@
+package com.edtech.quizz.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomePageController {
+    
+    @GetMapping({"/","/home"})
+    public String homePage(){
+        return "home";
+    }
+}
